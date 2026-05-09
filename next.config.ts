c88475f00fd6e7b04357054e39c3066188b8a9e1
ewang1027 <ethanwangbball@gmail.com>
2026-05-09 05:58:49 -0400

    finishing up some issues regarding memory allocation on the heap and dealing with Malloc Stack Logging errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["pdf-parse"],
+  experimental: {
+    optimizePackageImports: ["@react-three/drei"],
+  },
 };
 
 export default nextConfig;
