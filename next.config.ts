d254cb567ba6e1d873af5e4a8b27c4af726e24bb
ewang1027 <ethanwangbball@gmail.com>
2026-05-09 05:56:04 -0400

    having lots of memory allocation issues, my computer keeps crashing when working on it. The most recent one is spams of Malloc Stack Logging errors and I've just been trying to fix it for a while.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: __dirname,
-  },
+  serverExternalPackages: ["pdf-parse"],
 };
 
 export default nextConfig;
